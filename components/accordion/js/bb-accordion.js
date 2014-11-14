8370bdb7d4e4bdc7653836e5226c714c09ddc4c7
metbosch <boschpons@gmail.com>
2014-11-14 09:24:49 +0100

    mend

---
+++ components/accordion/js/bb-accordion.js
@@ -0,0 +1,17 @@
+/**
+  * BBAccordion decorator declaration
+  * @constructor
+  */
+var Decorate = function($provide) {
+    $provide.decorator('accordionGroupDirective', function($delegate) {
+        var directive = $delegate[0];
+        directive.templateUrl = "bb-accordion.tpl.html";
+        return $delegate;
+    });
+};
+
+
+/**
+  * Creation of 'bb.accordion' module
+  */
+angular.module('bb.accordion', ['ui.bootstrap']).config(['$provide', Decorate]);
