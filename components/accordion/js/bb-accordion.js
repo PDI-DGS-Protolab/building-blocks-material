b30112b64df9771bc115b1ed57def23a90544183
metbosch <boschpons@gmail.com>
2014-11-14 09:57:49 +0100

    Temp fix for accordion component

--- components/accordion/js/bb-accordion.js
+++ components/accordion/js/bb-accordion.js
@@ -5,7 +5,7 @@
 var Decorate = function($provide) {
     $provide.decorator('accordionGroupDirective', function($delegate) {
         var directive = $delegate[0];
-        directive.templateUrl = "../bb-accordion.tpl.html";
+        directive.templateUrl = "bb-accordion.tpl.html";
         return $delegate;
     });
 };
