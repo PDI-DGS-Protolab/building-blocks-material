0b18a4d3dd2498b0e75644c7594f6d2e2d1e8b75
metbosch <boschpons@gmail.com>
2014-11-17 11:02:41 +0100

    Relativized accordion template location. Updated accordion template and fixed minor bug in css

--- components/accordion/js/bb-accordion.js
+++ components/accordion/js/bb-accordion.js
@@ -1,11 +1,28 @@
 /**
+ * Save current script path to load template
+ * @type {string}
+ */
+var _bbAccordionDecorateCurrentScriptPath = (document.getElementsByTagName("script"))
+    [(document.getElementsByTagName("script")).length - 1].src;
+
+
+/**
   * BBAccordion decorator declaration
   * @constructor
   */
-var Decorate = function($provide) {
+var Decorate = function($provide, $sceDelegateProvider) {
+
+    var path = _bbAccordionDecorateCurrentScriptPath.substring(0, _bbAccordionDecorateCurrentScriptPath.lastIndexOf('/'));
+    path = path.substring(0, path.lastIndexOf('/') + 1);
+
+    var currentList = $sceDelegateProvider.resourceUrlWhitelist();
+    currentList =  currentList ? currentList : ['self'];
+    currentList.push(path + '**');
+    $sceDelegateProvider.resourceUrlWhitelist(currentList);
+
     $provide.decorator('accordionGroupDirective', function($delegate) {
         var directive = $delegate[0];
-        directive.templateUrl = "bb-accordion.tpl.html";
+        directive.templateUrl = path + 'bb-accordion.tpl.html';
         return $delegate;
     });
 };
@@ -14,4 +31,4 @@ var Decorate = function($provide) {
 /**
   * Creation of 'bb.accordion' module
   */
-angular.module('bb.accordion', ['ui.bootstrap']).config(['$provide', Decorate]);
+angular.module('bb.accordion', ['ui.bootstrap']).config(['$provide', '$sceDelegateProvider', Decorate]);
